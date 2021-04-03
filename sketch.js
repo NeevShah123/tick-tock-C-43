d2b18f23807517deb7a5896a2bf6f416cb35621b
NeevShah123 <73597414+NeevShah123@users.noreply.github.com>
2021-04-03 09:31:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var hour, minute, second;
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+  hr = hour();
+  mn = minute();
+  sc = second();
+}
+
+function draw() {
+  background(255,255,255);  
+
+  angleMode(DEGREES);
+  scAngle = map(sc,0,60,0,360);
+  drawSprites();
+}
